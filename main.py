912fea4bd536837003e71cdca179da9af01c9be5
ArekPolus <72617381+ArekPolus@users.noreply.github.com>
2021-12-30 17:18:56 +0000

    skrócenie do jednej linijki kodu

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # Write your code below this line
-input_len = input('What is your name? ')
-print(len(input_len))
+
+#input_len = input('What is your name? ')
+#rint(len(input_len))
+
+print(len(input("What is your name? ")))
